819874888948b4180c458f8629cae1b1e884e797
mishalturkane <mishalturkane@gmail.com>
2025-12-01 17:27:39 +0530

    generic function added

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,30 @@
-pub fn swap(a: i32, b: i32) -> [i32; 2] {
+pub fn swap<T: Copy>(a: T, b: T) -> [T; 2] {
     let (a, b) = (b, a); // built-in swap
     [a, b]
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn swap_test() {
+    fn swap_test_integers() {
         assert_eq!(swap(5, 9), [9, 5]);
     }
+
+    #[test]
+    fn swap_test_floats() {
+        assert_eq!(swap(1.2, 3.4), [3.4, 1.2]);
+    }
+
+    #[test]
+    fn swap_test_chars() {
+        assert_eq!(swap('a', 'z'), ['z', 'a']);
+    }
+    #[test]
+    fn swap_test_strings() {
+        assert_eq!(swap("hello", "world"), ["world", "hello"]);
+    }
 }
+
+
--- src/main.rs
+++
@@ -1,5 +0,0 @@
-use swap_crate::swap;
-fn main() {
-    let swapped = swap(33, 5);
-    println!("Swapped: {:?}", swapped); // Output: Swapped: [9, 5]
-}
